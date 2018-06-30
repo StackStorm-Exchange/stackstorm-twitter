9b46c869ddd9532372df1a1143508a5e4ad697b3
Nick Maludy <nmaludy@gmail.com>
2018-06-30 13:12:45 -0400

    Work on adding media upload support to update_status

--- actions/update_status.py
+++ actions/update_status.py
@@ -1,7 +1,13 @@
+from tempfile import NamedTemporaryFile
 from twitter import Twitter
 from twitter import OAuth
 
+import requests
+
 from st2common.runners.base_action import Action
+
+# 1 MB chunks
+DOWNLOAD_CHUNK_SIZE_BYTES = 1 * 1024 * 1024
 
 __all__ = [
     'UpdateStatusAction'
@@ -9,7 +15,24 @@ __all__ = [
 
 
 class UpdateStatusAction(Action):
-    def run(self, status):
+
+    def http_url_to_file(http_url):
+        data_file = NamedTemporaryFile()
+        req = requests.get(http, stream=True)
+        for chunk in req.iter_content(chunk_size=DOWNLOAD_CHUNK_SIZE_BYTES):
+            data_file.write(chunk)
+        return data_file
+
+    def read_media_file_or_url(media_path_or_url):
+        # If media_path_or_url is a URL, then download the file
+        # Else create a file object for the given path
+        if passed_media.startswith('http'):
+            data_file = http_url_to_file(media_path_or_url)
+        else:
+            data_file = open(os.path.realpath(media_path_or_url), 'rb')
+        return data_file.read()
+
+    def run(self, status, media):
         auth = OAuth(
             token=self.config['access_token'],
             token_secret=self.config['access_token_secret'],
@@ -17,6 +40,17 @@ class UpdateStatusAction(Action):
             consumer_secret=self.config['consumer_secret']
         )
         client = Twitter(auth=auth)
-        client.statuses.update(status=status)
+
+        if media:
+            media_ids = []
+            for m in media:
+                imagedata = read_media_file_or_url(m)
+                m_id = t_upload.media.upload(media=imagedata)["media_id_string"]
+                media_ids.append(m_id)
+
+            # - finally send tweet with the list of media ids:
+            client.statuses.update(status=status, media_ids=",".join(media_ids))
+        else:
+            client.statuses.update(status=status)
 
         return True
